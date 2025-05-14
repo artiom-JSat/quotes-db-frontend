08c84a3fe1b755e5b00de1bd92852fa0750a14bf
Artiom Sviridovich <jsat1987@gmail.com>
2025-05-14 15:08:10 +0200

    feature-1:init next app

--- client/app/page.js
+++ client/app/page.js
@@ -2,7 +2,6 @@ export default function Home() {
   return (
     <div className="flex items-start justify-center h-screen">
       <h2 className="mt-5 text-4xl text-center">Quotes Next.js application</h2>
-      <h3>biba</h3>
     </div>
   )
 }
