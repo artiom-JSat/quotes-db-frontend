a059477c45eebeb2ecd5c382737f01f9746c5266
Artiom Sviridovich <jsat1987@gmail.com>
2025-05-16 18:45:45 +0200

    feature-7: Added clear search button

--- client/components/Button.jsx
+++ client/components/Button.jsx
@@ -1,10 +1,16 @@
-export default function Button({ onClick, children }) {
+export default function Button({ onClick, children, variant = 'primary' }) {
+  // const baseClasses = "px-4 py-2 text-xl cursor-pointer bg-violet-500 text-white rounded-md hover:bg-violet-600 focus:outline-none focus:ring-2 focus:ring-violet-600"
+
+  const baseClasses =
+    'px-4 py-2 text-xl rounded-md cursor-pointer hover:bg-opacity-80 focus:outline-none focus:ring-2 focus:ring-opacity-50'
+  const variantClasses =
+    variant === 'primary'
+      ? 'bg-violet-800 text-white focus:ring-violet-600 hover:bg-violet-900'
+      : 'bg-gray-300 text-gray-800 focus:ring-gray-500 hover:bg-gray-400'
+
   return (
     <div className="text-center m-2">
-      <button
-        onClick={onClick}
-        className="px-4 py-2 text-xl cursor-pointer bg-violet-500 text-white rounded-md hover:bg-violet-600 focus:outline-none focus:ring-2 focus:ring-violet-600"
-      >
+      <button onClick={onClick} className={`${baseClasses} ${variantClasses}`}>
         {children}
       </button>
     </div>
