654d54654f94e1b26243e2bfe603cd4ddc1c2f60
Artiom Sviridovich <jsat1987@gmail.com>
2025-07-18 17:33:16 +0200

    feature-35: Created fetchQuotes and fetchRandomQuotes functions and get rid of default limits

--- client/app/(routes)/page.js
+++ client/app/(routes)/page.js
@@ -4,21 +4,22 @@ import { useEffect, useState } from 'react'
 import { ClipLoader } from 'react-spinners'
 import { Button } from '@components/Button'
 import { Quotes } from '@components/Quotes'
-import { fetchQuotes } from '@utils/quoteApiHandlers'
+import { fetchRandomQuotes } from '@utils/quoteApiHandlers'
 
 export default function RandomQuotesPage() {
   const [quotes, setQuotes] = useState([])
   const [isLoading, setIsLoading] = useState(true)
 
-  const fetchRandomQuotes = () => {
-    fetchQuotes({ setQuotes, setIsLoading })
-  }
-
-  useEffect(fetchRandomQuotes, [])
+  useEffect(() => {
+    fetchRandomQuotes({ setQuotes, setIsLoading })
+  }, [])
 
   return (
     <div className="p-4">
-      <Button onClick={fetchRandomQuotes} text="Get random quotes" />
+      <Button
+        onClick={() => fetchRandomQuotes({ setQuotes, setIsLoading })}
+        text="Get random quotes"
+      />
       {isLoading ? (
         <div className="flex justify-center items-center pt-20">
           <ClipLoader size={60} color="violet" />
